88d9e17a9ba934be8351b8864396e4235b8569b2
A01710217 <a01710217@tec.mx>
2024-05-07 19:51:10 -0600

    Actualizar la salida de promedio

--- promedio.py
+++ promedio.py
@@ -1,11 +1,10 @@
 #Promedio
 
-
 def promedio():
     n = int(input("Cuantos numeros vas a promediar? "))
     suma = 0
     for i in range(n):
         suma = suma + float(input("Dame un numero: "))
-    print("El promedio es: ", suma/n)
+    print(f"El promedio es: {suma/n}")
 
 promedio()
